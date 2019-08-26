14e110adaf118bbca5f7a0c04b4661042aa85682
Kenneth Okereke <kennethokereke@Kenneths-MacBook-Pro.local>
2019-08-26 12:14:08 -0400

    Garageparking

---
+++ Garage/theme.js
@@ -0,0 +1,18 @@
+const COLORS = {
+    purple: '#992EE8',
+    gray: '#7D818A',
+    black: '#3D4448',
+    white: '#FFFFFF',
+    overlay: '#9A9A9A',
+  };
+  
+  const SIZES = {
+    base: 12,
+    icon: 16,
+    font: 16,
+  }
+  
+  export {
+    COLORS,
+    SIZES,
+  }
